dd20b77c3f3df57bd366117a411efa91e56499e7
bKoZii <bigkonkamon@gmail.com>
2025-01-24 08:50:17 +0700

    เพิ่มไฟล์ inputTest.[svelte, vue]
    เพิ่มไฟล์ทดสอบ Reactive ของสองภาษา

---
+++ inputTest.svelte
@@ -0,0 +1,6 @@
+<script>
+	let name = $state('Konkamon');
+</script>
+
+<input type="text" bind:value={name} placeholder="Enter Your Name...">
+<h1>Hello, {name}!</h1>
---
+++ inputTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('Konkamon')
+</script>
+
+<template>
+  <input v-model="name" placeholder="Enter Your Name..." type="text" />
+  <h1>Hello, {{ name }}!</h1>
+</template>
